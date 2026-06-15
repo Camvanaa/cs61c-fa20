fa5d9aa5cee2597c9781ef999725df202aa05c08
Camvanaa <3499880997@qq.com>
2026-06-15 21:13:20 +0800

    feat(proj3): I&R type(?)

---
+++ Project/proj3/tests/part_b/custom/inputs/my-add.s
@@ -0,0 +1,8 @@
+add x1, x0, x0      # x1 = 0
+add x2, x0, x0      # x2 = 0
+add x3, x1, x2      # x3 = 0
+add x4, x3, x0      # x4 = 0
+add a0, x1, x0      # a0 = 0
+add a1, x2, x0      # a1 = 0
+add a2, x3, x0      # a2 = 0
+add a3, x4, x0      # a3 = 0
